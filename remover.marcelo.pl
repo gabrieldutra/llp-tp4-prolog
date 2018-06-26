deec52c1eb8524e5a012fd1a1a395ce9ef212f72
Gabriel Dutra <nesk.frz@gmail.com>
2018-06-25 21:32:06 -0300

    remover Marcelo

--- remover.marcelo.pl
+++ remover.marcelo.pl
@@ -1,3 +1,8 @@
-remover(_, [], []).
-remover(X, [X|T], B) :- remover(X, T, B).
-remover(X, [Y|T], [Y|NT]) :- X \= Y, remover(X, T, NT).
+concatenar([], L, L).
+concatenar([X|L1], L2, [X|L3]) :- concatenar(L1, L2, L3).
+
+remover(X, [X|T], T).
+% remover(X, [Y|T], NT) :- X =:= Y, remover(X, T, NT).
+% remover(X, [Y|T], [Y|NT]) :- X =\= Y, remover(X, T, NT).
+remover(X, [Y|T], NT) :- X =\= Y, concatenar(NT, [Y], NTY), remover(X, T, NTY).
+remover(X, [Y|T], NT) :- X =:= Y, remover(X, T, NT).
