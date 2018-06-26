fd37dc4fa249462e20ec4fbea899ecad0cfd7834
Gabriel Dutra <nesk.frz@gmail.com>
2018-06-25 21:35:44 -0300

    reverter done

--- reverter.pl
+++ reverter.pl
@@ -2,5 +2,4 @@ concatenar([], L, L).
 concatenar([X|L1], L2, [X|L3]) :- concatenar(L1, L2, L3).
 
 reverter([], []).
-reverter([A|LN], []) :- concatenar([], [A], LIN), reverter(LN, LIN).
-reverter([A|LN], LI) :- concatenar([A], LI, LIN), reverter(LN, LIN).
+reverter([A|X], B) :- reverter(X, C), concatenar(C, [A], B).
